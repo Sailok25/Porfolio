6a540f83e88e73590eb91aea55333285cfb1b7cc
Sailok <albaayvi4@gmail.com>
2024-12-23 02:00:29 +0100

    preparacion de inicio

---
+++ index.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/generales.css">
+    <title>Inicio</title>
+</head>
+
+
+<body>
+    <header></header>
+    <main>
+        <section id="presentacion"></section>
+        <section id="sobremi"></section>
+        <section id="proyectos"></section>
+        <section id="contacto"></section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
